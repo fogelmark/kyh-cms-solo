94345e4e75d0dc1b74542a0d8261a665bb595a44
fogelmark <108455196+fogelmark@users.noreply.github.com>
2023-06-02 19:34:33 +0200

    final 2

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 function ProtectedRoute({ user, children }) {
+  // Redirect to /login if you're not logged in.
   if (!user) {
     return < Navigate to="/login" replace />
   }
--- frontend/src/pages/__test__/Home.test.jsx
+++ frontend/src/pages/__test__/Home.test.jsx
@@ -3,7 +3,7 @@ import { describe, expect, it } from "vitest";
 import Home from "../Home";
 
 describe('Home', () => {
-
+  // Test to find the title 'CMS'
   it('should render the correct heading element', async () => {
     render(<Home />)
     const headingElement = screen.getByText('CMS')
